61983cdb1908fff7e87ee685e361397594848f32
Kiran Karkera <kiran.karkera@gmail.com>
2017-10-26 19:10:14 +0800

    updated to clojure  1.8

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "music compojure"
   :url "http://matthowlett.com"
   :dependencies [
-     [org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+     [org.clojure/clojure "1.8.0"]
+                 ;[org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 ])
